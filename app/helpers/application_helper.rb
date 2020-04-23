2570c3b932a7fcb9fff9c9854f46ffddbf9fa3d0
Baron Trieu <baron.trieu.goldenowl@gmail.com>
2020-04-23 18:16:17 +0700

    fix helper flash_type

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def alert_class_of_flash(flash_type)
-    { success: 'alert-success', error: 'alert-danger', alert: 'alert-warning', notice: 'alert-info' }.fetch(flash_type.to_sym, flash_type.to_s)
+    { success: 'alert-success', error: 'alert-danger', alert: 'alert-warning', notice: 'alert-info' }.fetch(flash_type.to_sym, 'alert-info')
   end
 end
