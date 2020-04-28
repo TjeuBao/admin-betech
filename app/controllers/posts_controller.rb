b30ea868a1e45917e8feaf58ae7f298612131973
Baron Trieu <baron.trieu.goldenowl@gmail.com>
2020-04-28 15:28:42 +0700

    add api json to index, show, create, update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,10 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all.order('created_at DESC')
+    respond_to do |format|
+      format.html
+      format.json { render json: @posts }
+    end
   end
 
   def new
@@ -11,14 +15,19 @@ class PostsController < ApplicationController
 
   def edit; end
 
-  def show; end
+  def show
+    respond_to do |format|
+      format.html
+      format.json { @post }
+    end
+  end
 
   def create
     @post = Post.new(post_params)
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
+        format.json { render @post, status: :created, location: @post }
       else
         format.html { render :new }
         format.json { render json: @post.errors, status: :unprocessable_entity }
@@ -30,7 +39,7 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.update(post_params)
         format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
+        format.json { render @post, status: :ok, location: @post }
       else
         format.html { render :edit }
         format.json { render json: @post.errors, status: :unprocessable_entity }
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
   def alert_class_of_flash(flash_type)
-    { success: 'alert-success', error: 'alert-danger', alert: 'alert-warning', notice: 'alert-info' }.fetch(flash_type.to_sym, 'alert-info')
+    {
+      success: 'alert-success',
+      error: 'alert-danger',
+      alert: 'alert-warning',
+      notice: 'alert-info'
+    }.fetch(flash_type.to_sym, 'alert-info')
   end
 end
