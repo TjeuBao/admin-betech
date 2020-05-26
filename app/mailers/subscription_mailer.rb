af3048f031bdd44353d4063bef9655f69eff5470
Baron Trieu <baron.trieu.goldenowl@gmail.com>
2020-05-26 10:12:40 +0700

    add template to send mail

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -1,2 +1,7 @@
 class SubscriptionMailer < ApplicationMailer
+  default from: 'from@example.com'
+
+  def email_subscription(list_email)
+    mail to: list_email, subject: 'Sample Email'
+  end
 end
