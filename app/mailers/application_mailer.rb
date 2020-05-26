c5f7d0243f6bbcc15a47e3f6954656146d45d05a
Baron Trieu <baron.trieu.goldenowl@gmail.com>
2020-05-27 00:35:30 +0700

    edit show user

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -4,7 +4,7 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'austin@goldenowl.asia'
   layout 'mailer'
 
-  def forget_password(user_email)
+  def reset_password_instructions(user_email)
     mail(to: user_email, subject: 'Forget Password Email') if user_email.any?
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module AdminGoldenowlAsia
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.action_mailer.default_url_options = { host: 'your.example.com' }
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
