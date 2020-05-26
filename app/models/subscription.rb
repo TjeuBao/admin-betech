ef6b8ba99a04d8d5d0198e433f228a99a9aef9c4
Baron Trieu <baron.trieu.goldenowl@gmail.com>
2020-05-26 10:11:42 +0700

    validates model subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -10,17 +10,6 @@
 #  updated_at        :datetime         not null
 #
 class Subscription < ApplicationRecord
-  after_create :send_mail_to_subscription
   validates :name, presence: true
   validates :email, presence: true
-
-  private
-  def send_mail_to_subscription
-    @subscription_email = Subscription.email if Subscription.subscription.type = nil
-		@subscription_email_post = Subscription.email if Subscription.subscription.type = post
-    @subscription_email_career = Subscription.email if Subscription.subscription.type = career
-
-    @post.create! => send mail to @subscription_email_post && send mail to subcription_email
-		@career.create!  => send mail to   @subscription_email_career && send mail to subcription_email
-  end
 end
