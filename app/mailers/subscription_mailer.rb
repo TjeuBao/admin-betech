e1d4ab46e5ce9f657c02b89c3b8d707b8903e54e
Baron Trieu <baron.trieu.goldenowl@gmail.com>
2020-05-26 11:19:26 +0700

    check mail to list_mail is any

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -2,6 +2,6 @@ class SubscriptionMailer < ApplicationMailer
   default from: 'from@example.com'
 
   def email_subscription(list_email)
-    mail to: list_email, subject: 'Sample Email'
+    mail(to: list_email, subject: 'Sample Email') if list_email.any?
   end
 end
--- spec/mailers/subscription_mailer_spec.rb
+++ spec/mailers/subscription_mailer_spec.rb
@@ -1,5 +1,3 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe SubscriptionMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+RSpec.describe SubscriptionMailer, type: :mailer
