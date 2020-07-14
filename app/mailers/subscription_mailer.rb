e9b24ac0b81cb3bf62304ec293879c0a05541072
liam-le-goldenowl <liam.le.goldenowl@gmail.com>
2020-07-14 15:37:29 +0700

    fix bug sent email subscription

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -1,7 +1,17 @@
 class SubscriptionMailer < ApplicationMailer
   default from: 'austin@goldenowl.asia'
 
-  def email_subscription(list_email)
-    mail(to: list_email, subject: 'Subscription Email') if list_email.any?
+  def subscription_email_for_post(email, post_id = nil)
+    @post = Post.find_by(id: post_id)
+    return if email.nil? || @post.nil?
+
+    mail(to: email, subject: 'Subscription Email')
+  end
+
+  def subscription_email_for_career(email, career_id = nil)
+    @career = Career.find_by(id: career_id)
+    return if email.nil? || @career.nil?
+
+    mail(to: email, subject: 'Subscription Email')
   end
 end
