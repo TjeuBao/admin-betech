d3b3fc05270986771be2b94743bbc936ddace833
liam-le <liam.le.goldenowl@gmail.com>
2020-07-22 03:49:11 +0700

    Get 3 related posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -13,8 +13,44 @@ module Api
       end
 
       def related_posts
-        @results = (Post.where('id > ?', @post.id).includes([:rich_text_content]).order(id: :desc).last(4) + [Post.where('id < ?', @post.id).includes([:rich_text_content]).order(id: :desc).first])
-        render json: PostSerializer.new(@results.compact)
+        t = 3
+
+        @new = Post.where('id > ?', @post.id)
+                   .includes([:rich_text_content])
+                   .order(id: :desc)
+                   .last(t)
+
+        @old = Post.where('id < ?', @post.id)
+                   .includes([:rich_text_content])
+                   .order(id: :desc)
+                   .last(t)
+
+        if @new.blank?
+          @old = Post.where('id < ?', @post.id)
+                     .includes([:rich_text_content])
+                     .order(id: :desc)
+                     .first(t)
+
+          render json: PostSerializer.new(@old)
+        elsif @old.blank?
+          render json: PostSerializer.new(@new)
+        elsif @old.length == @new.length
+          @old = Post.where('id < ?', @post.id)
+                     .includes([:rich_text_content])
+                     .order(id: :desc)
+                     .first(1)
+          @new.shift
+          render json: PostSerializer.new(@new + @old)
+        elsif (@old.length - @new.length).positive? # > 0
+          @old = Post.where('id < ?', @post.id)
+                     .includes([:rich_text_content])
+                     .order(id: :desc)
+                     .first(t)
+          render json: PostSerializer.new(@new + @old.shift(t - @new.length))
+        else (@old.length - @new.length).negative? # < 0
+          @new.shift(t - (t - 1))
+          render json: PostSerializer.new(@new + @old.shift(t - @new.length))
+        end
       end
 
       private
